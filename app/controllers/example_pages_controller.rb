791f98b064fac9b2e541b06365de45ffddb5cdc1
Peter Jang <peter.x.jang@gmail.com>
2018-06-24 09:53:44 -0500

    Add two more example routes

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -2,4 +2,12 @@ class ExamplePagesController < ApplicationController
   def hello_method
     render json: {peter: "is a cool guy!"}
   end
+
+  def test_method
+    render json: {message: "Yo", numbers: [3, 3, 2, 1]}
+  end
+
+  def nonjson_method
+    render html: "<h1>Hi</h1>".html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/hello_url" => "example_pages#hello_method"
+  get "/test_url" => "example_pages#test_method"
+  get "/nonjson_url" => "example_pages#nonjson_method"
 end
